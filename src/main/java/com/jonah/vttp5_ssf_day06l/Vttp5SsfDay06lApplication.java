910d4af03f99cf0ba31f56df97a26c0cc54456fb
jonahng <nyhj1998@gmail.com>
2024-11-28 09:41:37 +0800

    spring

---
+++ src/main/java/com/jonah/vttp5_ssf_day06l/Vttp5SsfDay06lApplication.java
@@ -0,0 +1,13 @@
+package com.jonah.vttp5_ssf_day06l;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vttp5SsfDay06lApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vttp5SsfDay06lApplication.class, args);
+	}
+
+}
